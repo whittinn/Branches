5c58e98b0a4d4479601144ac043053dd48a627b5
Nathaniel Whittington <whittinn@gmail.com>
2022-02-28 12:15:17 -0600

    Initial Commit

---
+++ Branches/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Branches
+//
+//  Created by Nathaniel Whittington on 2/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
